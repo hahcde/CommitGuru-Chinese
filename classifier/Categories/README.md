c3c9071e80b20557d3c93dd13d495dbe58421f15
Leandro Minku <minkull@bb-pg-login01.bear.cluster>
2019-07-22 22:19:21 +0100

    adding the capacity to consider keywords composed of words that have non-keywords between them when categorizing commits

--- classifier/Categories/README.md
+++ classifier/Categories/README.md
@@ -1,1 +1,3 @@
-This folder contains all the words used to categorize commits. Associated words with commits are expected to be comma-seperated and lower case. 
+This folder contains all the words used to categorize commits. Associated words with commits are expected to be comma-separated and lower case.
+Keywords composed of multiple words that allow non-keywords between them can be expressed using "...". For example, if a keyword requires both the words "repair" and "bug" to appear in the commit message, but other words are allowed to be between them (e.g., "repair pop-up bug"), the keyword can be expressed as "repair...bug".
+Chinese characters are also allowed. 
--- classifier/category.py
+++ classifier/category.py
@@ -27,6 +27,7 @@ class Category():
 			for row in wordreader:
 				for word in row:
 					self.associatedWords.append(word)
+		#print(self.associatedWords)
 
 	def belongs(self, commit_msg):
 		"""
@@ -34,13 +35,22 @@ class Category():
 		its commit message.
 		@return boolean
 		"""
-		commit_msg = commit_msg.lower().split(" ") # to array
+
+		commit_msg = commit_msg.lower()  #.split(" ") # to array <---le no real need to separate the commit message in words. In any case, assoc_words in word would result in any of the keywords associated to a give type of commit being within a word of the commit, rather than matching this word exactly. And, in Chinese, there are frequently no spaces between words.
 
 		# need to go beyond list contains i.e. fixed = fix
-		for word in commit_msg:
-			for assoc_word in self.associatedWords:
-				if assoc_word in word:
-					return True
+		#for word in commit_msg: <---le
+		for assoc_word in self.associatedWords:
+			assoc_word = assoc_word.split("...")  # <---le to enable keywords to have other words between them, e.g., 修改... 问题
+			found_keyword = True
+			for part_keyword in assoc_word: 
+			#if assoc_word in word:
+				if found_keyword and part_keyword in commit_msg:
+					found_keyword = True
+				else:
+					found_keyword = False
+			if found_keyword:
+				return True
 
 		# No associated words found!
 		return False
